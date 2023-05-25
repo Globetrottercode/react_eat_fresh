fcfc9d26a35111848f33b6dd4ebf4c65e35f2baf
Globetrottercode <dassouvik180@gmail.com>
2023-05-26 02:17:26 +0530

    Changes in layout in CheckAddr.js page

--- src/pages/CheckoutPages/CheckoutAddr.js
+++ src/pages/CheckoutPages/CheckoutAddr.js
@@ -11,25 +11,49 @@ function CheckoutAddr() {
             <h3>CHOOSE ADDRESS</h3>
           </div>
           <div className={checkout.checkoutAddressCard}>
+            <div>
+              <input
+                onClick={(e) => {
+                  console.log(e.target.value);
+                }}
+                type="radio"
+                id="age1"
+                name="age"
+                value="30"
+              />
+            </div>
+
+            <br />
             <input
               onClick={(e) => {
                 console.log(e.target.value);
               }}
               type="radio"
-              id="age1"
+              id="age2"
               name="age"
-              value="30"
+              value="60"
             />
-            <label for="age1">0 - 30</label>
-            <br />
-            <input type="radio" id="age2" name="age" value="60" />
             <label for="age2">31 - 60</label>
             <br />
-            <input type="radio" id="age3" name="age" value="100" />
+            <input
+              onClick={(e) => {
+                console.log(e.target.value);
+              }}
+              type="radio"
+              id="age3"
+              name="age"
+              value="100"
+            />
             <label for="age3">61 - 100</label>
             <br />
             <br />
-            <input type="submit" value="Submit" />
+            <input
+              onClick={(e) => {
+                console.log(e.target.value);
+              }}
+              type="submit"
+              value="Submit"
+            />
           </div>
         </div>
         <div className={checkout.checkoutAddrRight}></div>
