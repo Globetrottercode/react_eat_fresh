88ce1b01d4b268ae6bd1f75d71dbc0d188a5763c
Globetrottercode <dassouvik180@gmail.com>
2023-05-26 17:45:34 +0530

    Final layout made for CheckoutAddr.js

--- src/pages/CheckoutPages/CheckoutAddr.js
+++ src/pages/CheckoutPages/CheckoutAddr.js
@@ -1,6 +1,7 @@
 import TopNavbar from "../../components/TopNavbar";
 import Footer from "../../components/Footer";
 import checkout from "../../css/checkout.module.css";
+import "../../css/styles.css";
 function CheckoutAddr() {
   return (
     <>
@@ -79,16 +80,37 @@ function CheckoutAddr() {
               <div>
                 <p>Save As : </p>
               </div>
-              <input placeholder="Complete Address" type="text" />
-            </div>
-            <div className={checkout.checkoutAddAddressCardLine2}>
-              <input placeholder="Floor(Optional)" type="text" />
-            </div>
-            <div className={checkout.checkoutAddAddressCardLine3}>
               <input placeholder="Home" type="text" />
             </div>
-            <div className={checkout.checkoutAddAddressCardLine4}></div>
-            <div className={checkout.checkoutAddAddressCardLine5}></div>
+            <div className={checkout.checkoutAddAddressCardLine2}>
+              <input placeholder="Complete Address" type="text" />
+            </div>
+            <div className={checkout.checkoutAddAddressCardLine3}>
+              <input placeholder="Floor(Optional)" type="text" />
+            </div>
+            <div className={checkout.checkoutAddAddressCardLine4}>
+              <input placeholder="Landmark" type="text" />
+            </div>
+            <div className={checkout.checkoutAddAddressCardLine5}>
+              <select
+                // ref={ref}
+                className="form-select cityDropDown"
+                name="cars"
+                id="days"
+              >
+                <option value="Bangalore">Bangalore</option>
+                <option value="Mumbai">Mumbai</option>
+                <option value="Pune">Pune</option>
+                <option value="Hyderabad"> Hyderabad</option>
+                <option value="Kolkata">Kolkata</option>
+                <option value="Delhi"> Delhi</option>
+                <option value="Noida">Noida</option>
+                <option value="Chennai"> Chennai</option>
+                <option value="Gurgaon">Gurgaon</option>
+                <option value="Ahmedabad">Ahmedabad</option>
+              </select>
+              <input type="text" placeholder="Pincode" />
+            </div>
           </div>
           <div type="button" className={checkout.checkoutAddBtn}>
             <h3>Add</h3>
