0887bd9a5f4130c1f0abaec5461f28fd64a38f08
Globetrottercode <dassouvik180@gmail.com>
2023-06-06 23:53:57 +0530

    Created updateEmail.js to update email of users

--- src/getData/getUser.js
+++ src/getData/getUser.js
@@ -1,14 +1,17 @@
 async function getUser(username) {
   // return empty array if no plan else returns an array of plans
-  const response = await fetch(`http://localhost:3500/getUsers/${username}`, {
-    // credentials: 'include',
-    // Origin:"http://localhost:3000/login",
+  const response = await fetch(
+    `http://localhost:3500/customer/getUsers/${username}`,
+    {
+      // credentials: 'include',
+      // Origin:"http://localhost:3000/login",
 
-    method: "GET",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
+      method: "GET",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
   const json = await response.json();
   if (response.status === 200) {
     if (!json[0]) return {};
