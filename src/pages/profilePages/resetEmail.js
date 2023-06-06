a9a1b73514975932dfe9624408ecf6ec478f546a
Globetrottercode <dassouvik180@gmail.com>
2023-06-06 21:00:45 +0530

    ResetEmail.js page created

--- src/pages/profilePages/resetEmail.js
+++ src/pages/profilePages/resetEmail.js
@@ -1,11 +1,24 @@
 import TopNavbar from "../../components/TopNavbar";
 import Footer from "../../components/Footer";
+import LOGO from "../../images/LOGO.png";
+import "../../css/styles.css";
 
 function ResetEmail() {
   return (
     <>
       <TopNavbar />
-      <div></div>
+      <div className="resetBody">
+        <img style={{ height: "30%" }} src={LOGO} alt="logo" />
+        <div className="resetCard">
+          <div className="resetLabel">
+            <p>Reset Email</p>
+          </div>
+          <input className="resetInput" placeholder="New Email"></input>
+          <div className="resetButton">
+            <p>Reset</p>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
