513cb1edc03ba3bd76d0c31dce9fdcadb4197076
Globetrottercode <dassouvik180@gmail.com>
2023-06-07 00:26:31 +0530

    added the update Email feature working fine

---
+++ src/getData/updateName.js
@@ -0,0 +1,29 @@
+async function updateName(username, name) {
+  // returns -1 if no credits created else returns an integer value of credit
+  const response = await fetch("http://localhost:3500/customer/updateName", {
+    // credentials: 'include',
+    // Origin:"http://localhost:3000/login",
+
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      username: username,
+      name: name,
+    }),
+  });
+  const json = await response.json();
+  //   console.log(json);
+  if (response.status === 200) {
+    return json;
+  }
+}
+
+async function check() {
+  console.log(await updateName("dasbiplob023@gmail.com", "Biplob Das"));
+}
+
+check();
+
+// export default updateName;
--- src/pages/profilePages/resetEmail.js
+++ src/pages/profilePages/resetEmail.js
@@ -1,9 +1,32 @@
 import TopNavbar from "../../components/TopNavbar";
 import Footer from "../../components/Footer";
 import LOGO from "../../images/LOGO.png";
+import updateEmail from "../../getData/updateEmail";
+import { useState } from "react";
+import * as EmailValidator from "email-validator";
+import getUser from "../../getData/getUser";
+import { notify } from "../../alerts/toastify";
+import { useNavigate } from "react-router-dom";
 import "../../css/styles.css";
 
 function ResetEmail() {
+  let navigate = useNavigate();
+  let [email, setEmail] = useState("");
+  async function handleReset() {
+    if (!EmailValidator.validate(email)) {
+      notify("Invalid mail-id !");
+      return;
+    }
+    let result = await updateEmail(localStorage.getItem("username"), email);
+    if (result[0].username === email) {
+      localStorage.setItem("username", email);
+      // console.log(localStorage.getItem("username"));
+      notify("Email updated succesfully !");
+      let user = await getUser(localStorage.getItem("username"));
+      localStorage.setItem("user", JSON.stringify(user));
+      navigate("/profile");
+    }
+  }
   return (
     <>
       <TopNavbar />
@@ -13,8 +36,15 @@ function ResetEmail() {
           <div className="resetLabel">
             <p>Reset Email</p>
           </div>
-          <input className="resetInput" placeholder="New Email"></input>
-          <div className="resetButton">
+          <input
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            value={email}
+            className="resetInput"
+            placeholder="New Email"
+          ></input>
+          <div onClick={handleReset} className="resetButton">
             <p>Reset</p>
           </div>
         </div>
