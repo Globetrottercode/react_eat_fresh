9eef3b688aa8a7699cadc47b47182c8a070989f3
Globetrottercode <dassouvik180@gmail.com>
2023-06-08 18:51:00 +0530

    Added eye for reset password

--- src/pages/profilePages/resetPassword.js
+++ src/pages/profilePages/resetPassword.js
@@ -8,6 +8,8 @@ import { notify } from "../../alerts/toastify";
 import { useNavigate } from "react-router-dom";
 
 function ResetPassword() {
+  let [isOldOn, setIsOldOn] = useState(false);
+  let [isNewOn, setIsNewOn] = useState(false);
   let navigate = useNavigate();
   let [oldPass, setOldPass] = useState("");
   let [newPass, setNewPass] = useState("");
@@ -26,6 +28,12 @@ function ResetPassword() {
       navigate("/profile");
     }
   }
+  function handleOldEye() {
+    setIsOldOn(!isOldOn);
+  }
+  function handleNewEye() {
+    setIsNewOn(!isNewOn);
+  }
   return (
     <>
       <TopNavbar />
@@ -35,22 +43,105 @@ function ResetPassword() {
           <div className="resetLabel">
             <p>Reset Password</p>
           </div>
-          <input
-            onChange={(e) => {
-              setOldPass(e.target.value);
-            }}
-            className="resetInput"
-            value={oldPass}
-            placeholder="Old Password"
-          ></input>
-          <input
-            onChange={(e) => {
-              setNewPass(e.target.value);
-            }}
-            className="resetInput"
-            value={newPass}
-            placeholder="New Password"
-          ></input>
+          {isOldOn ? (
+            <div className="resetInputPassDiv">
+              <input
+                type="text"
+                onChange={(e) => {
+                  setOldPass(e.target.value);
+                }}
+                className="resetInputPass"
+                value={oldPass}
+                placeholder="Old Password"
+              ></input>
+              <div
+                onClick={handleOldEye}
+                style={{ backgroundColor: "rgba(99, 47, 107, 0.5" }}
+                className="eye"
+              >
+                <i class="fa-sharp fa-regular fa-eye"></i>
+              </div>
+            </div>
+          ) : (
+            <div className="resetInputPassDiv">
+              <input
+                type="password"
+                onChange={(e) => {
+                  setOldPass(e.target.value);
+                }}
+                className="resetInputPass"
+                value={oldPass}
+                placeholder="Old Password"
+              ></input>
+              <div
+                onClick={handleOldEye}
+                style={{ backgroundColor: "rgba(99, 47, 107, 0.5" }}
+                className="eye"
+              >
+                <i class="fa-sharp fa-regular fa-eye-slash"></i>
+              </div>
+            </div>
+          )}
+          {isNewOn ? (
+            <div className="resetInputPassDiv">
+              {" "}
+              <input
+                type="text"
+                onChange={(e) => {
+                  setNewPass(e.target.value);
+                }}
+                className="resetInputPass"
+                value={newPass}
+                placeholder="New Password"
+              ></input>
+              <div
+                onClick={handleNewEye}
+                style={{ backgroundColor: "rgba(99, 47, 107, 0.5" }}
+                className="eye"
+              >
+                <i class="fa-sharp fa-regular fa-eye"></i>
+              </div>
+            </div>
+          ) : (
+            <div className="resetInputPassDiv">
+              {" "}
+              <input
+                type="password"
+                onChange={(e) => {
+                  setNewPass(e.target.value);
+                }}
+                className="resetInputPass"
+                value={newPass}
+                placeholder="New Password"
+              ></input>
+              <div
+                onClick={handleNewEye}
+                style={{ backgroundColor: "rgba(99, 47, 107, 0.5" }}
+                className="eye"
+              >
+                <i class="fa-sharp fa-regular fa-eye-slash"></i>
+              </div>
+            </div>
+          )}
+          {/* <div className="resetInputPassDiv">
+            {" "}
+            <input
+              type="password"
+              onChange={(e) => {
+                setNewPass(e.target.value);
+              }}
+              className="resetInputPass"
+              value={newPass}
+              placeholder="New Password"
+            ></input>
+            <div
+              style={{ backgroundColor: "rgba(99, 47, 107, 0.5" }}
+              className="eye"
+            >
+              <i class="fa-sharp fa-regular fa-eye-slash"></i>
+            </div>
+          </div> */}
+
           <div onClick={handleReset} className="resetButton">
             <p>Reset</p>
           </div>
